f433241ce82b8aa4149976fb3fd306e46349d8bf
Mahmoud Elboery <mahmoudbubg1234@gmail.com>
2025-09-30 04:14:29 +0300

    simplify Spring Security configuration by removing unused imports and redundant code

--- src/main/java/com/nexaworks/rafiq/config/SecurityConfig.java
+++ src/main/java/com/nexaworks/rafiq/config/SecurityConfig.java
@@ -1,22 +1,12 @@
 package com.nexaworks.rafiq.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
-
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -28,7 +18,5 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-
 
 }
