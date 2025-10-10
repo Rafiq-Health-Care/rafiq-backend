586712b0d2f558986a71a7c854ae690e6079520e
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-10-10 15:15:03 +0300

    feat: add Role management support with services and repository
    - Create `RoleRepository` with `findByName` method for role retrieval.
    - Define `Roles` enum for standard role naming.
    - Implement `RoleService` and its `RoleServiceImpl` for role handling.
    - Introduce `RegistrationException` for custom exception handling during registration.

---
+++ src/main/java/com/nexaworks/rafiq/service/ServiceImpl/RoleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.nexaworks.rafiq.service.ServiceImpl;
+
+import com.nexaworks.rafiq.entities.Role;
+import com.nexaworks.rafiq.enums.Roles;
+import com.nexaworks.rafiq.repository.RoleRepository;
+import com.nexaworks.rafiq.service.RoleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+    @Override
+    public Role getRole(Roles roles) {
+        return roleRepository.findByName(roles.toString());
+    }
+}
