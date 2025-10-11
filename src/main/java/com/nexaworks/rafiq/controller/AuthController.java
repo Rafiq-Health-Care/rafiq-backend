2bc53bb3fdab167483c79cbed3f1922693e05da8
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-10-11 23:56:36 +0300

    refactor: organize DTO imports and update package structure
    - Moved `ResetPasswordRequest` to `dto.request` package.
    - Removed unused imports across several classes (`AuthService`, `AuthController`, `AuthServiceImpl`, etc.).
    - Optimized imports for better readability and package organization.

--- src/main/java/com/nexaworks/rafiq/controller/AuthController.java
+++ src/main/java/com/nexaworks/rafiq/controller/AuthController.java
@@ -1,7 +1,5 @@
 package com.nexaworks.rafiq.controller;
 
-import com.nexaworks.rafiq.dto.*;
-
 import com.nexaworks.rafiq.dto.request.*;
 import com.nexaworks.rafiq.dto.response.LoginResponse;
 import com.nexaworks.rafiq.dto.response.VerifyOtpResponse;
