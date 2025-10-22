5a67f7f1eb3d503ca9a9dfb192c6fbb04b41e29c
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-10-23 01:13:35 +0300

    feat: implement global and validation exception handling

---
+++ src/main/java/com/nexaworks/rafiq/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.nexaworks.rafiq.exception.handler;
+
+
+import com.nexaworks.rafiq.exception.model.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.MethodNotAllowedException;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleMethodNotAllowed(HttpRequestMethodNotSupportedException ex, HttpServletRequest request) {
+        return ResponseEntity.status(405).body(new ErrorResponse(405, "Method Not Allowed",ex.getMessage(), LocalDateTime.now(),request.getRequestURI()));
+    }
+
+}
