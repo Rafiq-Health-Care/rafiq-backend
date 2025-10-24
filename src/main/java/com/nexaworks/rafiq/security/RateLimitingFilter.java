b123e5be3b08ddda301acbd26bb05a7bbd87dad6
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-10-24 11:04:03 +0300

    feat: enhance RateLimitingFilter to return JSON response for rate limit exceeded

--- src/main/java/com/nexaworks/rafiq/security/RateLimitingFilter.java
+++ src/main/java/com/nexaworks/rafiq/security/RateLimitingFilter.java
@@ -4,7 +4,9 @@ import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -35,9 +37,13 @@ public class RateLimitingFilter implements Filter {
         if (bucket.tryConsume(1)){
             filterChain.doFilter(servletRequest, servletResponse);
         } else {
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            response.setContentType("application/json");
+            response.getWriter().write(
+                    "{\"message\":\"Rate limit exceeded. Try again later\"}"
+            );
 
-            servletResponse.getWriter().write("Too many requests - Rate limit exceeded");
-            servletResponse.getWriter().flush();
         }
 
     }
