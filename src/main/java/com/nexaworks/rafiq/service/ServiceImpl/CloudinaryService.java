56a61d4b8ccb527e76c02619f1bc8b5ca7eff38d
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-10-26 19:17:59 +0300

    feat: enhance CloudinaryService with upload resource functionality and error handling

--- src/main/java/com/nexaworks/rafiq/service/ServiceImpl/CloudinaryService.java
+++ src/main/java/com/nexaworks/rafiq/service/ServiceImpl/CloudinaryService.java
@@ -14,8 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
--- src/test/java/com/nexaworks/rafiq/service/JwtServiceImplTest.java
+++ src/test/java/com/nexaworks/rafiq/service/JwtServiceImplTest.java
@@ -19,7 +19,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import javax.crypto.SecretKey;
+
 import java.util.Collection;
 import java.util.List;
 
