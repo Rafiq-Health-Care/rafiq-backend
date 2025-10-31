d80f9d9e638a86950638d92d2fe3225c6254d7a2
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-10-31 15:08:18 +0200

    refactor: remove unused RefreshRequest DTO

--- src/main/java/com/nexaworks/rafiq/dto/request/RefreshRequest.java
+++
@@ -1,7 +0,0 @@
-package com.nexaworks.rafiq.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record RefreshRequest(@NotBlank
-                             String refreshToken) {
-}
