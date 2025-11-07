03e0499d9942ce887c9a2432157a12ecbc9d75e3
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-11-07 19:52:50 +0200

    test: enhance UserServiceImplTest with additional password update scenarios
    - Added a test for successful password update when the old password matches.
    - Added a test to verify exception handling when the old password does not match.

--- src/test/java/com/nexaworks/rafiq/service/UserServiceImplTest.java
+++ src/test/java/com/nexaworks/rafiq/service/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package com.nexaworks.rafiq.service;
 
 import com.nexaworks.rafiq.dto.request.ResetPasswordRequest;
 import com.nexaworks.rafiq.entities.User;
+import com.nexaworks.rafiq.exception.custom.InvalidPasswordException;
 import com.nexaworks.rafiq.repository.UserRepository;
 import com.nexaworks.rafiq.service.ServiceImpl.*;
 import com.nexaworks.rafiq.utils.AuthSessionManager;
@@ -14,6 +15,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
@@ -45,7 +47,7 @@ public class UserServiceImplTest {
     }
     @DisplayName("Update password should encode and save the new password")
     @Test
-    void updatePassword_ShouldEncodeAndSaveNewPassword() {
+    void updatePassword_ShouldEncodeAndSaveNewPassword_WhenOldPasswordMatches() {
         User user = User.builder().id(java.util.UUID.randomUUID()).build();
         ResetPasswordRequest resetPasswordRequest = new ResetPasswordRequest("123","123");
         when(passwordEncoder.matches(any(),any())).thenReturn(true);
@@ -53,6 +55,16 @@ public class UserServiceImplTest {
         when(userRepository.save(user)).thenReturn(user);
         userService.updatePassword(user,resetPasswordRequest);
         verify(userRepository,times(1)).save(user);
+    }
+    @DisplayName("Update password should throw exception if old password doesn't match")
+    @Test
+    void updatePassword_ShouldThrowException_WhenOldPasswordDoesNotMatch() {
+        User user = User.builder().id(java.util.UUID.randomUUID()).build();
+        ResetPasswordRequest resetPasswordRequest = new ResetPasswordRequest("123","123");
+        when(passwordEncoder.matches(any(),any())).thenReturn(false);
+        when(passwordEncoder.encode(anyString())).thenReturn("123");
+        assertThrows(InvalidPasswordException.class,()->userService.updatePassword(user,resetPasswordRequest));
+        verify(userRepository,never()).save(user);
     }
 
 }
