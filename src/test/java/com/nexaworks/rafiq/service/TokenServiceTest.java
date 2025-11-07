8b698d5a41df28886b2d844cb587473ddccb9d65
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-11-07 17:11:48 +0200

    test: add unit tests for TokenServiceImpl to cover token generation and OTP verification scenarios

--- src/test/java/com/nexaworks/rafiq/service/TokenServiceTest.java
+++ src/test/java/com/nexaworks/rafiq/service/TokenServiceTest.java
@@ -3,7 +3,10 @@ package com.nexaworks.rafiq.service;
 import com.nexaworks.rafiq.entities.Token;
 import com.nexaworks.rafiq.entities.User;
 import com.nexaworks.rafiq.enums.TokenType;
+import com.nexaworks.rafiq.exception.custom.TokenInvalidException;
+import com.nexaworks.rafiq.exception.custom.TokenNotFoundException;
 import com.nexaworks.rafiq.exception.custom.UserException;
+import com.nexaworks.rafiq.exception.custom.UserNotFoundException;
 import com.nexaworks.rafiq.repository.TokenRepository;
 import com.nexaworks.rafiq.service.ServiceImpl.TokenServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,8 +15,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.Optional;
 import java.util.UUID;
 
 
@@ -26,11 +31,14 @@ public class TokenServiceTest {
     @Mock
     TokenRepository tokenRepository;
     @InjectMocks
+    @Spy
     TokenServiceImpl tokenService;
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
         ReflectionTestUtils.setField(tokenService, "REFRESH_EXPIRATION", 3600L);
+        ReflectionTestUtils.setField(tokenService, "OTP_EXPIRATION", 3600L);
+        ReflectionTestUtils.setField(tokenService, "ACCESS_TOKEN_EXPIRATION", 3600L);
     }
 
     @DisplayName("Generate refresh token should return token and save to repository")
@@ -64,4 +72,70 @@ public class TokenServiceTest {
         assertEquals(refreshToken,token.getToken());
         assertEquals(TokenType.REFRESH,token.getTokenType());
     }
+
+    @DisplayName("Generate access token should return and save access token")
+    @Test
+    void generateAccessToken_ShouldReturnAndSaveAccessToken(){
+        User user = User.builder().id(UUID.randomUUID()).email("test@example.com").build();
+        when(tokenRepository.save(any(Token.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        Token fakeToken = new Token();
+        fakeToken.setToken("fake-access-token");
+        doReturn(fakeToken)
+                .when(tokenService)
+                .buildToken(eq(user), anyString(), any(TokenType.class), anyLong());
+        String accessToken = tokenService.generateAccessToken(Optional.of(user));
+        assertNotNull(accessToken);
+        verify(tokenRepository,times(1)).save(any(Token.class));
+    }
+
+    @DisplayName("Generate access token should throw exception when user is null")
+    @Test
+    void generateAccessToken_ShouldThrowException_WhenUserIsNull(){
+      assertThrows(UserNotFoundException.class, () -> tokenService.generateAccessToken(Optional.empty()));
+      verify(tokenRepository, never()).save(any(Token.class));
+    }
+
+    @DisplayName("Verify otp should return the user when otp is valid")
+    @Test
+    void verifyOtp_ShouldReturnTheUser_WhenOtpIsValid() {
+        User user = User.builder().firstName("John").lastName("Doe").build();
+        user.setEmail("john@gmail.com");
+        Token token = new Token();
+        token.setUser(user);
+        token.setToken("123456");
+        token.setTokenType(TokenType.OTP);
+        token.setExpiryDate(java.time.Instant.now().plusSeconds(3600));
+        when(tokenRepository.findByToken(anyString())).thenReturn(Optional.of(token));
+        User verifiedUser = tokenService.verifyOtp(user.getEmail(), "123456");
+        assertNotNull(verifiedUser);
+        assertEquals(user,verifiedUser);
+
+    }
+    @DisplayName("Verify otp should throw exception when no otp token match")
+    @Test
+    void verifyOtp_ShouldThrowException_WhenNoOtpTokenMatch() {
+        when(tokenRepository.findByToken(anyString())).thenReturn(Optional.empty());
+        assertThrows(TokenNotFoundException.class,
+                () -> tokenService.verifyOtp("john@gmail.com", "1234"));
+        verify(tokenRepository, times(1)).findByToken(anyString());
+    }
+    @DisplayName("Verify otp should throw exception when the otp is expired")
+    @Test
+    void verifyOtp_shouldThrowException_WhenOtpTokenIsExpired(){
+        User user = User.builder().firstName("John").lastName("Doe").build();
+        user.setEmail("john@gmail.com");
+        Token token = new Token();
+        token.setUser(user);
+        token.setToken("123456");
+        token.setTokenType(TokenType.OTP);
+        token.setExpiryDate(java.time.Instant.now());
+        when(tokenRepository.findByToken(anyString())).thenReturn(Optional.of(token));
+        assertThrows(TokenInvalidException.class,
+                () -> tokenService.verifyOtp(user.getEmail(), "123456"));
+        verify(tokenRepository, times(1)).findByToken(anyString());
+
+
+
+    }
+
 }
