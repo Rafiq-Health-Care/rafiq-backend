05105b926aea4c0efe5525ce27fdf4c0e3e198da
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-11-14 16:03:29 +0200

    test

--- src/test/java/com/nexaworks/rafiq/integration/controller/SpecializationControllerIntegrationTest.java
+++ src/test/java/com/nexaworks/rafiq/integration/controller/SpecializationControllerIntegrationTest.java
@@ -1,28 +1,19 @@
 package com.nexaworks.rafiq.integration.controller;
 
-import com.nexaworks.rafiq.integration.BaseIntegrationTest;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-
-
 import static org.hamcrest.Matchers.greaterThan;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.nexaworks.rafiq.integration.BaseIntegrationTest;
 
 @DisplayName("Specialization Controller Integration Testing")
 public class SpecializationControllerIntegrationTest extends BaseIntegrationTest {
-
 
     @Autowired
     private MockMvc mockMvc;
@@ -30,11 +21,9 @@ public class SpecializationControllerIntegrationTest extends BaseIntegrationTest
     @Test
     @DisplayName("Get Specialization List")
     public void getSpecializationList() throws Exception {
-         mockMvc.perform(MockMvcRequestBuilders.get("/specialization"))
-                .andExpect(status().isOk())
+        mockMvc.perform(MockMvcRequestBuilders.get("/specialization")).andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$.length()").value(greaterThan(0)))
-                .andExpect(jsonPath("$[0].id").exists())
-                .andExpect(jsonPath("$[0].name").exists());
+                .andExpect(jsonPath("$[0].id").exists()).andExpect(jsonPath("$[0].name").exists());
     }
 }
