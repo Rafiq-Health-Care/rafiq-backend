2a5042efa1af452ff6dd909f4c86d2133ddf26d5
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-11-19 16:59:56 +0200

    refactor: migrate mappers to MapStruct for improved efficiency and simplicity
    - Replaced manual mapping in `TestMapper`, `AddressMapper`, and `ResultMapper` with MapStruct.
    - Added `@Mapper` annotations and mapping configurations for streamlined object transformations.
    - Removed redundant imports and components for cleaner code.

--- src/main/java/com/nexaworks/rafiq/mapper/ResultMapper.java
+++ src/main/java/com/nexaworks/rafiq/mapper/ResultMapper.java
@@ -1,21 +1,17 @@
 package com.nexaworks.rafiq.mapper;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.nexaworks.rafiq.dto.request.TestRequest;
 import com.nexaworks.rafiq.entities.LabResult;
 
-@Component
-public class ResultMapper {
-    public List<LabResult> toEntity(List<TestRequest> results) {
-        if (results == null || results.isEmpty()) {
-            return Collections.emptyList();
-        }
-        return results.stream().map(r -> LabResult.builder().name(r.testName()).result(r.result())
-                .unit(r.unit()).status(r.status()).build()).collect(Collectors.toList());
-    }
+@Mapper(componentModel = "spring")
+public interface ResultMapper {
+    @Mapping(source = "testName", target = "name")
+    LabResult toEntity(TestRequest request);
+
+    List<LabResult> toEntity(List<TestRequest> results);
 }
--- src/main/java/com/nexaworks/rafiq/mapper/TestMapper.java
+++ src/main/java/com/nexaworks/rafiq/mapper/TestMapper.java
@@ -1,19 +1,14 @@
 package com.nexaworks.rafiq.mapper;
 
-import java.util.Date;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-import org.springframework.stereotype.Component;
-
-import com.nexaworks.rafiq.dto.request.TestRequest;
 import com.nexaworks.rafiq.dto.response.TestResultsResponse;
 import com.nexaworks.rafiq.entities.LabTest;
 
-@Component
-public class TestMapper {
-    public TestResultsResponse mapToTestResponse(LabTest test) {
-        return new TestResultsResponse(test.getName(), test.getId(), test.getPdf(),
-                test.getFileType(), Date.from(test.getDate()),
-                test.getLabResults().stream().map(t -> new TestRequest(t.getName(), t.getResult(),
-                        t.getUnit(), t.getStatus())).toList());
-    }
+@Mapper(componentModel = "spring")
+public interface TestMapper {
+    @Mapping(source = "id", target = "testId")
+    @Mapping(source = "labResults", target = "tests")
+    TestResultsResponse mapToTestResponse(LabTest test);
 }
