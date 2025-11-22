dd90e1b67ae4126f2beadd0cbd23d42598c687c6
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-11-22 20:22:59 +0200

    feat: enhance Reminder entity with custom days and status attributes

--- src/main/java/com/nexaworks/rafiq/entities/Reminder.java
+++ src/main/java/com/nexaworks/rafiq/entities/Reminder.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.nexaworks.rafiq.enums.Day;
 import com.nexaworks.rafiq.enums.ReminderFrequency;
+import com.nexaworks.rafiq.enums.ReminderStatus;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -25,10 +26,17 @@ public class Reminder extends BaseEntity {
     private int minute;
     @Enumerated(EnumType.STRING)
     private ReminderFrequency frequency;
+    // todo review this
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "custom_days", joinColumns = @JoinColumn(name = "reminder_id"))
+    @Column(name = "day")
+    @Enumerated(EnumType.STRING)
     private List<Day> customDays;
     private boolean vibrate;
     private Instant startDate;
     private Instant endDate;
+    @Enumerated(EnumType.STRING)
+    private ReminderStatus status;
     @ManyToOne
     @JoinColumn(name = "medicine_id", referencedColumnName = "id", nullable = false)
     private Medicine medicine;
@@ -36,7 +44,7 @@ public class Reminder extends BaseEntity {
     @JoinColumn(name = "patient_id", referencedColumnName = "id", nullable = false)
     private PatientProfile patient;
     @ManyToOne
-    @JoinColumn(name = "group_id", referencedColumnName = "id", nullable = true)
+    @JoinColumn(name = "group_id", referencedColumnName = "id")
     private Group group;
     @OneToMany(mappedBy = "reminder")
     private List<ReminderLog> reminderLogs;
