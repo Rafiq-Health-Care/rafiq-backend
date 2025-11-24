676b7066b3226fa7a2535641503c0c0ca19e2338
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-11-24 12:11:03 +0200

    fix: extend application tests with BaseIntegrationTest for consistent setup

--- src/test/java/com/nexaworks/rafiq/RafiqApplicationTests.java
+++ src/test/java/com/nexaworks/rafiq/RafiqApplicationTests.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.nexaworks.rafiq.integration.BaseIntegrationTest;
+
 @SpringBootTest
 @ActiveProfiles("test")
-class RafiqApplicationTests {
+class RafiqApplicationTests extends BaseIntegrationTest {
 
     @Test
     void contextLoads() {
