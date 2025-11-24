0c2d05d30cc5e26296ed4285c6b36d6e076634a1
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-11-24 12:02:16 +0200

    fix: set active profile to "test" in application tests

--- src/test/java/com/nexaworks/rafiq/RafiqApplicationTests.java
+++ src/test/java/com/nexaworks/rafiq/RafiqApplicationTests.java
@@ -2,8 +2,10 @@ package com.nexaworks.rafiq;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RafiqApplicationTests {
 
     @Test
