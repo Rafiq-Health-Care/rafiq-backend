c4ddb879be4e1eab602c60948d332162ecdc02a5
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-04 12:20:09 +0300

    Enhance RabbitMQ configuration:
    - Add manual acknowledgment and retry logic to RabbitMQ properties.
    - Introduce dead-letter queues (DLQs) and bindings for email, SMS, and push notifications.
    - Update existing queues with DLQ configuration for improved error handling.
    
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/config/RabbitMQConfig.java
+++ src/main/java/com/nexaworks/rafiq/config/RabbitMQConfig.java
@@ -21,17 +21,74 @@ public class RabbitMQConfig {
     public static final String ROUTING_KEY_SMS = "notification.sms";
     public static final String ROUTING_KEY_PUSH = "notification.push";
 
+    public static final String EMAIL_DLQ = "notification.email.dlq";
+    public static final String SMS_DLQ = "notification.sms.dlq";
+    public static final String PUSH_DLQ = "notification.push.dlq";
+
+    public static final String NOTIFICATION_DLQ_EXCHANGE = "notification.dlq.exchange";
+
+
+    @Bean(name = "emailDLQ")
+    public Queue emailDLQ() {
+        return new Queue(EMAIL_DLQ, true);
+    }
+    @Bean(name = "smsDLQ")
+    public Queue smsDLQ() {
+        return new Queue(SMS_DLQ, true);
+    }
+    @Bean(name = "pushDLQ")
+    public Queue pushDLQ() {
+        return new Queue(PUSH_DLQ, true);
+    }
+    @Bean(name = "notificationDLQExchange")
+    public DirectExchange notificationDLQExchange() {
+        return new DirectExchange(NOTIFICATION_DLQ_EXCHANGE);
+    }
+
+    @Bean
+    public Binding emailDLQBinding(@Qualifier("notificationDLQExchange") DirectExchange exchange,@Qualifier("emailDLQ") Queue queue) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY_EMAIL);
+    }
+    @Bean
+    public Binding smsDLQBinding(@Qualifier("notificationDLQExchange")DirectExchange exchange,@Qualifier("smsDLQ") Queue queue) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY_SMS);
+    }
+    @Bean
+    public Binding pushDLQBinding(@Qualifier("notificationDLQExchange")DirectExchange exchange,@Qualifier("pushDLQ")Queue queue) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY_PUSH);
+    }
+
     @Bean(name = "emailQueue")
     public Queue emailQueue() {
-        return new Queue(EMAIL_NOTIFICATION_QUEUE, true);
+        return QueueBuilder.durable(EMAIL_NOTIFICATION_QUEUE)
+                .withArgument("x-dead-letter-exchange", NOTIFICATION_DLQ_EXCHANGE)
+                .withArgument("x-dead-letter-routing-key", ROUTING_KEY_EMAIL)
+                .build();
     }
+
     @Bean(name = "smsQueue")
     public Queue smsQueue() {
-        return new Queue(SMS_NOTIFICATION_QUEUE, true);
+        return QueueBuilder.durable(SMS_NOTIFICATION_QUEUE)
+                .withArgument("x-dead-letter-exchange", NOTIFICATION_DLQ_EXCHANGE)
+                .withArgument("x-dead-letter-routing-key", ROUTING_KEY_SMS)
+                .build();
     }
+
     @Bean(name = "pushQueue")
     public Queue pushQueue() {
-        return new Queue(PUSH_NOTIFICATION_QUEUE, true);
+        return QueueBuilder.durable(PUSH_NOTIFICATION_QUEUE)
+                .withArgument("x-dead-letter-exchange", NOTIFICATION_DLQ_EXCHANGE)
+                .withArgument("x-dead-letter-routing-key", ROUTING_KEY_PUSH)
+                .build();
     }
 
     @Bean(name = "notificationExchange")
