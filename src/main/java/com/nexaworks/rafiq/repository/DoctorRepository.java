f29b375b7a930c60aef381d37dbd3459fff538e6
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-10 19:27:15 +0300

    Add pageable `findBySpecialization` method to `DoctorRepository`
    - Introduced `findBySpecialization` method with `Pageable` support for paginated queries.
    - Updated imports to include `Page` and `Specialization` dependencies.
    
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/repository/DoctorRepository.java
+++ src/main/java/com/nexaworks/rafiq/repository/DoctorRepository.java
@@ -2,11 +2,14 @@ package com.nexaworks.rafiq.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import com.nexaworks.rafiq.entities.Doctor;
+import com.nexaworks.rafiq.entities.enums.Specialization;
 
 import jakarta.persistence.LockModeType;
 
@@ -14,4 +17,6 @@ public interface DoctorRepository extends JpaRepository<Doctor, UUID> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT d FROM Doctor d WHERE d.id = :id")
     void findByIdWithLock(UUID id);
+
+    Page<Doctor> findBySpecialization(Specialization specialization, Pageable pageable);
 }
