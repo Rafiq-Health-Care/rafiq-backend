5968cae9c30b09b5a7c6baaa67416320deb851b3
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-10 19:15:56 +0300

    Add `Education` and `Experience` entities, and extend `Doctor` entity with related fields
    - Introduced `Education` and `Experience` entities with Jackson and Lombok annotations.
    - Extended `Doctor` entity to include `biography`, `education`, and `experience` fields with JSONB support.
    
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

---
+++ src/main/java/com/nexaworks/rafiq/entities/Education.java
@@ -0,0 +1,20 @@
+package com.nexaworks.rafiq.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Education {
+    private String degree;
+    private String university;
+    private Integer sartYear;
+    private Integer endYear;
+}
---
+++ src/main/java/com/nexaworks/rafiq/entities/Experience.java
@@ -0,0 +1,21 @@
+package com.nexaworks.rafiq.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Experience {
+    private String position;
+    private String hospital;
+    private Integer sartYear;
+    private Integer endYear;
+    private String description;
+}
