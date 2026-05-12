cad79271a5290fce659331c668f02d70beaf0f8c
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-13 00:14:47 +0300

    Add `ConsultationSummaryMapper` for mapping between DTOs and `ConsultationSummary` entity
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

---
+++ src/main/java/com/nexaworks/rafiq/mapper/ConsultationSummaryMapper.java
@@ -0,0 +1,50 @@
+package com.nexaworks.rafiq.mapper;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import com.nexaworks.rafiq.dto.request.summary.CreateConsultationSummaryRequest;
+import com.nexaworks.rafiq.dto.request.summary.UpdateConsultationSummaryRequest;
+import com.nexaworks.rafiq.dto.response.summary.ConsultationSummaryResponse;
+import com.nexaworks.rafiq.entities.ConsultationSummary;
+
+@Mapper(componentModel = "spring")
+public interface ConsultationSummaryMapper {
+
+    @BeanMapping(ignoreUnmappedSourceProperties = "consultationId")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "doctor", ignore = true)
+    @Mapping(target = "patient", ignore = true)
+    @Mapping(target = "consultation", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
+    @Mapping(target = "deletedBy", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    ConsultationSummary toEntity(CreateConsultationSummaryRequest request);
+
+    @Mapping(target = "consultationId", source = "consultation.id")
+    @Mapping(target = "doctorId", source = "doctor.id")
+    @Mapping(target = "patientId", source = "patient.id")
+    ConsultationSummaryResponse toResponse(ConsultationSummary entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "doctor", ignore = true)
+    @Mapping(target = "patient", ignore = true)
+    @Mapping(target = "consultation", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
+    @Mapping(target = "deletedBy", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntity(UpdateConsultationSummaryRequest request,
+            @MappingTarget ConsultationSummary entity);
+}
