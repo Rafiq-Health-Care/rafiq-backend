f74ebe1560d4e086ff6b86c38e18ef35c04b9453
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-13 16:17:58 +0300

    Add error code `R02` to `RtcProviderException`
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/exception/custom/RtcProviderException.java
+++ src/main/java/com/nexaworks/rafiq/exception/custom/RtcProviderException.java
@@ -1,6 +1,7 @@
 package com.nexaworks.rafiq.exception.custom;
 
 public class RtcProviderException extends RuntimeException {
+    public static final String CODE = "R02";
     public RtcProviderException(String message) {
         super(message);
     }
