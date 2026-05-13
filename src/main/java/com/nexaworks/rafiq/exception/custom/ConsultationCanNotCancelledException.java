6c3878e40f88e3cbcac2fd2f8c606f3d785c1d04
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-13 16:17:41 +0300

    Add custom exceptions for consultation-related errors
    - Introduce `ConsultationNotFoundException` with error code `C01`.
    - Implement `ConsultationReservedException` with error code `C02`.
    - Add `ConsultationOverlappingException` with error code `C03`.
    - Create `ConsultationCanNotCancelledException` with error code `C04`.
    - Define `ConsultationCanNotEditException` with error code `C05`.
    
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

---
+++ src/main/java/com/nexaworks/rafiq/exception/custom/ConsultationCanNotCancelledException.java
@@ -0,0 +1,7 @@
+package com.nexaworks.rafiq.exception.custom;
+
+public class ConsultationCanNotCancelledException extends RuntimeException {
+    public static final String CODE = "C04";
+    public ConsultationCanNotCancelledException(String s) {
+    }
+}
