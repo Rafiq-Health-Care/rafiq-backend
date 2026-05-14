fe0ab9f93f6779362fbdeee7f4072d8e59c65111
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-14 11:15:33 +0300

    Introduce `RefundRequest` entity and `RefundStatus` enum
    - Add `RefundRequest` entity with fields for status, amount, payment, patient, and consultation relationships.
    - Define `RefundStatus` enum with values: PENDING, COMPLETED, CANCELLED, REFUSED.
    
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

---
+++ src/main/java/com/nexaworks/rafiq/entities/RefundRequest.java
@@ -0,0 +1,41 @@
+package com.nexaworks.rafiq.entities;
+
+import java.math.BigDecimal;
+
+import com.nexaworks.rafiq.entities.enums.RefundStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString(onlyExplicitlyIncluded = true)
+@Entity
+
+public class RefundRequest extends BaseEntity {
+
+    @ToString.Include
+    @Builder.Default
+    private RefundStatus status = RefundStatus.PENDING;
+    @ToString.Include
+    private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "payment_id", nullable = false)
+    private Payment payment;
+
+    @OneToOne
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    @OneToOne
+    @JoinColumn(name = "consultation_id", nullable = false)
+    private Consultation consultation;
+
+}
---
+++ src/main/java/com/nexaworks/rafiq/entities/enums/RefundStatus.java
@@ -0,0 +1,5 @@
+package com.nexaworks.rafiq.entities.enums;
+
+public enum RefundStatus {
+    PENDING, COMPLETED, CANCELLED, REFUSED
+}
