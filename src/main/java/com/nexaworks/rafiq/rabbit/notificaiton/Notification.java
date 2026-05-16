9d3e13806e7a0c90b7e2f9ad89a20fc3ecf4546f
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-16 13:44:17 +0300

    Update package paths for `EmailNotification` and `Notification` to align with RabbitMQ package structure
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/rabbit/notificaiton/Notification.java
+++ src/main/java/com/nexaworks/rafiq/rabbit/notificaiton/Notification.java
@@ -1,4 +1,4 @@
-package com.nexaworks.rafiq.dto.notificaiton;
+package com.nexaworks.rafiq.rabbit.notificaiton;
 
 public sealed interface Notification permits EmailNotification, SmsNotification, PushNotification {
 }
