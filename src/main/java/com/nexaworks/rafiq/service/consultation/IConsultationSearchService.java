7f0e46348566a0a34048770309a9bc555ec7f044
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-16 20:14:06 +0300

    Refactor `IConsultationSearchService` to use `ConsultationSlot` for methods and update return types
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/service/consultation/IConsultationSearchService.java
+++ src/main/java/com/nexaworks/rafiq/service/consultation/IConsultationSearchService.java
@@ -7,24 +7,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.nexaworks.rafiq.dto.request.consultation.ScheduleFilter;
-import com.nexaworks.rafiq.dto.response.consultation.CallResponse;
-import com.nexaworks.rafiq.dto.response.consultation.ConsultationFilter;
 import com.nexaworks.rafiq.dto.response.consultation.DoctorConsultationResponse;
 import com.nexaworks.rafiq.entities.Consultation;
+import com.nexaworks.rafiq.entities.ConsultationSlot;
 import com.nexaworks.rafiq.entities.enums.ConsultationStatus;
 
 public interface IConsultationSearchService {
-    Page<Consultation> getConsultations(ConsultationFilter filter, Pageable pageable);
 
-    CallResponse getConsultationCall(UUID id);
+    ConsultationSlot getConsultation(UUID id);
 
-    Consultation getConsultation(UUID id);
-
-    Page<Consultation> getDoctorSchedule(ScheduleFilter filter, Pageable pageable);
+    Page<ConsultationSlot> getDoctorSchedule(ScheduleFilter filter, Pageable pageable);
 
     List<Consultation> getPatientUpcoming();
 
-    List<Consultation> getDoctorUpcoming();
+    List<ConsultationSlot> getDoctorUpcoming();
 
     List<Consultation> getPatientConsultation(ConsultationStatus status);
 
