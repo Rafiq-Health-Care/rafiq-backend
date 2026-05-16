640002e376ea0f1e187ff892500100fd336c85ed
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-16 20:16:04 +0300

    Introduce `ConsultationSlotRepository` for slot and consultation management
    - Add methods for overlapping consultation slot checks.
    - Retrieve upcoming and available consultation slots for doctors.
    
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

---
+++ src/main/java/com/nexaworks/rafiq/repository/ConsultationSlotRepository.java
@@ -0,0 +1,51 @@
+package com.nexaworks.rafiq.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.nexaworks.rafiq.dto.response.consultation.DoctorConsultationResponse;
+import com.nexaworks.rafiq.entities.ConsultationSlot;
+import com.nexaworks.rafiq.entities.enums.ConsultationStatus;
+import com.nexaworks.rafiq.entities.enums.SlotStatus;
+
+public interface ConsultationSlotRepository
+        extends
+            JpaRepository<ConsultationSlot, UUID>,
+            JpaSpecificationExecutor<ConsultationSlot> {
+    @Query("""
+            SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END
+            FROM ConsultationSlot c
+            WHERE c.doctor.id = :doctorId
+            AND c.status != :status
+            AND c.startTime < :end
+            AND c.endTime > :start
+            """)
+    boolean existsByOverlapping(LocalDateTime startTime, LocalDateTime endTime, UUID id,
+            ConsultationStatus consultationStatus);
+
+    @Query("""
+            SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END
+            FROM ConsultationSlot c
+            WHERE c.doctor.id = :doctorId
+            AND c.id != :consultationId
+            AND c.status != :status
+            AND c.startTime < :end
+            AND c.endTime > :start
+            """)
+    boolean existsByOverlapping(@Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end, @Param("doctorId") UUID userId,
+            @Param("consultationId") UUID id, @Param("status") ConsultationStatus status);
+
+    @Query("SELECT new com.nexaworks.rafiq.dto.response.consultation.DoctorConsultationResponse(c.id,c.startTime,c.endTime) FROM ConsultationSlot  c where c.doctor.id = :id AND c.status = :status")
+    List<DoctorConsultationResponse> getDoctorAvailableConsultation(UUID id,
+            ConsultationStatus consultationStatus);
+
+    @Query("SELECT c FROM ConsultationSlot c WHERE c.doctor.id = :doctorId AND c.status = :status")
+    List<ConsultationSlot> findAllDoctorUpcoming(UUID doctorId, SlotStatus slotStatus);
+}
