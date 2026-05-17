b4344f576a430d652e8e89fa1f75c6a2affbb8fd
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-17 22:30:24 +0300

    Remove notification sending logic from `ConsultationPreparationService` and update `prepare` method to return access token
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/service/consultation/ConsultationPreparationService.java
+++ src/main/java/com/nexaworks/rafiq/service/consultation/ConsultationPreparationService.java
@@ -2,23 +2,16 @@ package com.nexaworks.rafiq.service.consultation;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 import com.nexaworks.rafiq.entities.Consultation;
-import com.nexaworks.rafiq.entities.Doctor;
-import com.nexaworks.rafiq.entities.Patient;
-import com.nexaworks.rafiq.entities.enums.ActionStatus;
 import com.nexaworks.rafiq.entities.enums.ConsultationStatus;
 import com.nexaworks.rafiq.exception.custom.ConsultationNotFoundException;
 import com.nexaworks.rafiq.exception.custom.RtcProviderException;
-import com.nexaworks.rafiq.rabbit.notificaiton.PushNotification;
 import com.nexaworks.rafiq.repository.ConsultationRepository;
 import com.nexaworks.rafiq.service.call.RtcProvider;
-import com.nexaworks.rafiq.service.notification.NotificationService;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -28,18 +21,17 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class ConsultationPreparationService implements IConsultationPreparationService {
-    private final NotificationService<PushNotification> notificationService;
     private final ConsultationRepository consultationRepository;
     private final RtcProvider rtcProvider;
 
     @Override
     @Transactional(rollbackOn = {RtcProviderException.class})
-    public void prepare(UUID uuid) {
+    public String prepare(UUID uuid) {
         Consultation consultation = consultationRepository.findConsultationById(uuid)
                 .orElseThrow(() -> new ConsultationNotFoundException("Consultation not found"));
 
         if (!consultation.getStatus().isPreparable()) {
-            return;
+            return null;
         }
         int expiration = Math.toIntExact(LocalDateTime.now()
                 .until(consultation.getTimeSlot().getEndTime(), ChronoUnit.SECONDS));
@@ -54,24 +46,6 @@ public class ConsultationPreparationService implements IConsultationPreparationS
         consultation.setStatus(ConsultationStatus.LIVE);
         consultation.setAccessToken(accessToken);
         consultationRepository.save(consultation);
-
-        sendNotificationsToPatientAndDoctor(consultation, accessToken);
-    }
-
-    private void sendNotificationsToPatientAndDoctor(Consultation consultation,
-            String accessToken) {
-        Map<String, Object> data = new HashMap<>();
-        Doctor doctor = consultation.getDoctor();
-        Patient patient = consultation.getPatient();
-        data.put("doctorName", doctor.getName());
-        data.put("patientName", patient.getName());
-        data.put("token", accessToken);
-        data.put("consultationId", consultation.getId().toString());
-        PushNotification patientNotification = new PushNotification(
-                ActionStatus.CONSULTATION_COMING_UP, patient.getNotificationToken(), data);
-        notificationService.sendNotification(patientNotification);
-        PushNotification doctorNotification = new PushNotification(
-                ActionStatus.CONSULTATION_COMING_UP, doctor.getNotificationToken(), data);
-        notificationService.sendNotification(doctorNotification);
+        return accessToken;
     }
 }
