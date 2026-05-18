a4fff43bcbcf7a065c34157103bdf9996d558904
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-18 11:07:12 +0300

    Refactor `ConsultationRepository` to simplify overlapping query methods and remove unused queries
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/repository/ConsultationRepository.java
+++ src/main/java/com/nexaworks/rafiq/repository/ConsultationRepository.java
@@ -13,7 +13,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.nexaworks.rafiq.dto.response.consultation.CallResponse;
-import com.nexaworks.rafiq.dto.response.consultation.DoctorConsultationResponse;
 import com.nexaworks.rafiq.entities.Consultation;
 import com.nexaworks.rafiq.entities.enums.ConsultationStatus;
 
@@ -23,17 +22,6 @@ public interface ConsultationRepository
         extends
             JpaRepository<Consultation, UUID>,
             JpaSpecificationExecutor<Consultation> {
-    @Query("""
-            SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END
-            FROM Consultation c
-            WHERE c.doctor.id = :doctorId
-            AND c.status != :status
-            AND c.timeSlot.startTime < :end
-            AND c.timeSlot.endTime > :start
-            """)
-    boolean existsByOverlapping(@Param("start") LocalDateTime startTime,
-            @Param("end") LocalDateTime endTime, @Param("doctorId") UUID doctorId,
-            @Param("status") ConsultationStatus status);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Consultation> findConsultationById(UUID id);
@@ -41,26 +29,13 @@ public interface ConsultationRepository
     @Query("""
             SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END
             FROM Consultation c
-            WHERE c.doctor.id = :doctorId
-            AND c.id != :consultationId
-            AND c.status != :status
-            AND c.timeSlot.startTime < :end
-            AND c.timeSlot.endTime > :start
-            """)
-    boolean existsByOverlapping(@Param("start") LocalDateTime start,
-            @Param("end") LocalDateTime end, @Param("doctorId") UUID userId,
-            @Param("consultationId") UUID id, @Param("status") ConsultationStatus status);
-    @Query("""
-            SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END
-            FROM Consultation c
-            WHERE c.doctor.id = :patientId
-            AND c.status != :status
-            AND c.timeSlot.startTime < :end
-            AND c.timeSlot.endTime > :start
+            WHERE c.patient.id = :patientId
+            AND c.status != ConsultationStatus.CANCELLED
+            AND c.slot.startTime < :end
+            AND c.slot.endTime > :start
             """)
     boolean existsByPatientOverlapping(@Param("start") LocalDateTime startTime,
-            @Param("end") LocalDateTime endTime, @Param("patientId") UUID id1,
-            @Param("status") ConsultationStatus consultationStatus);
+            @Param("end") LocalDateTime endTime, @Param("patientId") UUID id1);
 
     @Query("SELECT new com.nexaworks.rafiq.dto.response.consultation.CallResponse(c.id,c.accessToken) FROM Consultation c")
     CallResponse getConsultationCallInfo(UUID id);
@@ -70,23 +45,14 @@ public interface ConsultationRepository
             @Param("status1") ConsultationStatus status1,
             @Param("status2") ConsultationStatus status2);
 
-    @Query("SELECT c FROM Consultation c WHERE c.doctor.id= :doctorId AND c.status= :status1 OR c.status= :status2")
-    List<Consultation> findAllDoctorUpcoming(@Param("doctorId") UUID doctorId,
-            @Param("status1") ConsultationStatus status1,
-            @Param("status2") ConsultationStatus status2);
-
     @Query("SELECT c FROM Consultation c WHERE c.patient.id = :patientId AND c.status= :status")
     List<Consultation> findAllPatientConsultation(@Param("patientId") UUID authenticateUserId,
             @Param("status") ConsultationStatus status);
 
-    @Query("SELECT new com.nexaworks.rafiq.dto.response.consultation.DoctorConsultationResponse(c.id,c.timeSlot.startTime,c.timeSlot.endTime) FROM Consultation  c where c.doctor.id = :id AND c.status = :status")
-    List<DoctorConsultationResponse> getDoctorAvailableConsultation(@Param("id") UUID id,
-            @Param("status") ConsultationStatus consultationStatus);
-
     @Query("""
             SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END
             FROM Consultation c
-            WHERE c.doctor.id = :doctorId
+            WHERE c.slot.id = :doctorId
             AND c.patient.id = :patientId
             AND c.status NOT IN :excludedStatuses
             """)
