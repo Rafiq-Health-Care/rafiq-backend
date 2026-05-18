4e4d62aa96cdce9cccee22834cd72831eb22e5ce
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-18 12:19:23 +0300

    Modify `IRefundService` to return `UUID` and support partial refunds with `isFullRefund` flag
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/service/refund/IRefundService.java
+++ src/main/java/com/nexaworks/rafiq/service/refund/IRefundService.java
@@ -1,7 +1,9 @@
 package com.nexaworks.rafiq.service.refund;
 
+import java.util.UUID;
+
 import com.nexaworks.rafiq.entities.Consultation;
 
 public interface IRefundService {
-    void refund(Consultation consultation);
+    UUID refund(Consultation consultation, boolean isFullRefund);
 }
