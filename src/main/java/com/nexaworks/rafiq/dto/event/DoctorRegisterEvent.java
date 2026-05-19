11b2ad3cf761ea91ace119be2e9dffc9dbf32496
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-19 23:33:02 +0300

    Remove unused event DTOs under `com.nexaworks.rafiq.dto.event`.
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/dto/event/DoctorRegisterEvent.java
+++
@@ -1,9 +0,0 @@
-package com.nexaworks.rafiq.dto.event;
-
-import java.util.UUID;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public record DoctorRegisterEvent(UserRegistrationEvent event, UUID doctorId,
-        MultipartFile nationalId) {
-}
