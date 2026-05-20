393170add7b8f0fa6a1f45b7ffac2bd043a94f3a
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-20 09:14:52 +0300

    Refactor `DrugController` and `DrugService`: remove mapper dependencies from the controller, update `search` method to return service-layer DTO responses directly.
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/service/medicine/DrugServiceImpl.java
+++ src/main/java/com/nexaworks/rafiq/service/medicine/DrugServiceImpl.java
@@ -7,7 +7,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.nexaworks.rafiq.dto.response.common.PageResponse;
+import com.nexaworks.rafiq.dto.response.medicine.DrugSearchResponse;
 import com.nexaworks.rafiq.entities.Drug;
+import com.nexaworks.rafiq.mapper.DrugMapper;
+import com.nexaworks.rafiq.mapper.PageMapper;
 import com.nexaworks.rafiq.repository.DrugRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,10 +22,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DrugServiceImpl implements DrugService {
     private final DrugRepository drugRepository;
+    private final PageMapper pageMapper;
+    private final DrugMapper drugMapper;
     @Override
-    public Page<Drug> search(String drugName, int page, int size) {
+    public PageResponse<DrugSearchResponse> search(String drugName, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
-        return drugRepository.searchByFullText(drugName, pageable);
+        Page<Drug> drugs = drugRepository.searchByFullText(drugName, pageable);
+        return pageMapper.mapToDrugSearchResponsePage(drugs, drugMapper);
     }
 
     @Override
