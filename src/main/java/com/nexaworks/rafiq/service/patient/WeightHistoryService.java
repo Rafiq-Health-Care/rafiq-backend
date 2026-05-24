c18591561b72a2836317e9fd7e4a075f51973fe4
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-24 20:35:22 +0300

    Remove `WeightHistory` feature: delete related entity, controller, repository, service, and implementation.
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/service/patient/WeightHistoryService.java
+++
@@ -1,9 +0,0 @@
-package com.nexaworks.rafiq.service.patient;
-
-import com.nexaworks.rafiq.entities.Patient;
-
-import jakarta.validation.constraints.Positive;
-
-public interface WeightHistoryService {
-    void logNewWeight(@Positive Double weight, Patient patient);
-}
