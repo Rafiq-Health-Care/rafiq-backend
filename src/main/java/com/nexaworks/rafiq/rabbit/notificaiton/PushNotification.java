7350c7d9c8200f182915012cdce60bd14d9a6911
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-25 14:34:18 +0300

    Refactor `PushNotification` and `NotificationService`: update field types and method signatures, add `FirebaseMessagingException` handling.
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/rabbit/notificaiton/PushNotification.java
+++ src/main/java/com/nexaworks/rafiq/rabbit/notificaiton/PushNotification.java
@@ -4,6 +4,6 @@ import java.util.Map;
 
 import com.nexaworks.rafiq.entities.enums.ActionStatus;
 
-public record PushNotification(ActionStatus title, String ft, String body,
-        Map<String, Object> data) implements Notification {
+public record PushNotification(ActionStatus action, String ft, String body,
+        Map<String, String> data) implements Notification {
 }
