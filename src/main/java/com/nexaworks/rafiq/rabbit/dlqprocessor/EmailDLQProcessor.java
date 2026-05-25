9b82913e13d24dc2a08ae6f497b945b4a63f307b
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-25 22:12:32 +0300

    Remove message logging components: delete `MessageLog`, `MessageLogRepository`, `IMessageLogService`, and related service logic.
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/rabbit/dlqprocessor/EmailDLQProcessor.java
+++ src/main/java/com/nexaworks/rafiq/rabbit/dlqprocessor/EmailDLQProcessor.java
@@ -7,14 +7,11 @@ import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nexaworks.rafiq.entities.enums.Level;
-import com.nexaworks.rafiq.rabbit.constant.RabbitMQConstant;
 import com.nexaworks.rafiq.rabbit.enums.DLQAction;
 import com.nexaworks.rafiq.rabbit.handler.IRetryHandler;
 import com.nexaworks.rafiq.rabbit.notificaiton.EmailNotification;
 import com.nexaworks.rafiq.service.alert.AlertService;
-import com.nexaworks.rafiq.service.messagelog.IMessageLogService;
 import com.rabbitmq.client.Channel;
 
 import lombok.RequiredArgsConstructor;
@@ -26,15 +23,10 @@ import lombok.extern.slf4j.Slf4j;
 public class EmailDLQProcessor {
     private final AlertService alertService;
     private final IRetryHandler retryHandler;
-    private final IMessageLogService messageLogService;
-    private final ObjectMapper objectMapper;
 
     public void processMessage(String failureReason, EmailNotification notification,
             Channel channel, Map<String, Object> headers) throws IOException {
         DLQAction action = classify(failureReason);
-        messageLogService.persist(failureReason, objectMapper.writeValueAsString(notification),
-                headers, action, notification.notificationId(),
-                RabbitMQConstant.EMAIL_NOTIFICATION_QUEUE);
 
         switch (action) {
 
