52ff27ca2aba59f3ef183804413345eb182dca06
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-27 01:25:38 +0300

    Add notification management methods to `NotificationPersistenceService` and `NotificationRepository`
    - Introduce methods for marking notifications as read, deleting notifications, and fetching user-specific notifications with pagination.
    - Integrate `AuthService` and `NotificationMapper` for user authentication and response mapping.
    
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/service/notification/NotificationPersistenceService.java
+++ src/main/java/com/nexaworks/rafiq/service/notification/NotificationPersistenceService.java
@@ -1,12 +1,20 @@
 package com.nexaworks.rafiq.service.notification;
 
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.nexaworks.rafiq.dto.response.common.PageResponse;
+import com.nexaworks.rafiq.dto.response.notification.NotificationResponse;
 import com.nexaworks.rafiq.entities.Notification;
 import com.nexaworks.rafiq.entities.User;
+import com.nexaworks.rafiq.mapper.NotificationMapper;
 import com.nexaworks.rafiq.rabbit.notificaiton.PushNotification;
 import com.nexaworks.rafiq.repository.NotificationRepository;
+import com.nexaworks.rafiq.service.authentication.AuthService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +24,8 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class NotificationPersistenceService implements INotificationPersistenceService {
     private final NotificationRepository notificationRepository;
+    private final AuthService authService;
+    private final NotificationMapper mapper;
 
     @Override
     @Transactional
@@ -24,5 +34,36 @@ public class NotificationPersistenceService implements INotificationPersistenceS
                 .user(user).title(notification.action().getTitle()).message(notification.body())
                 .data(notification.data()).build();
         notificationRepository.save(savedNotification);
+    }
+
+    @Override
+    public void markNotificationAsRead(UUID id) {
+        notificationRepository.markNotificationAsRead(id);
+    }
+
+    @Override
+    public void deleteNotification(UUID id) {
+        notificationRepository.deleteById(id);
+    }
+
+    @Override
+    public void markAllAsRead() {
+        UUID userId = authService.getAuthenticateUserId();
+        notificationRepository.markAllAsReadByUserId(userId);
+
+    }
+
+    @Override
+    public PageResponse<NotificationResponse> getAll(Pageable pageable) {
+        UUID userId = authService.getAuthenticateUserId();
+        Page<Notification> notifications = notificationRepository.getAllByUserId(userId, pageable);
+        return PageResponse.of(notifications, mapper::toResponse);
+    }
+
+    @Override
+    public NotificationResponse getById(UUID id) {
+        Notification notification = notificationRepository.findById(id)
+                .orElseThrow(() -> new NullPointerException("Notification not found"));
+        return mapper.toResponse(notification);
     }
 }
