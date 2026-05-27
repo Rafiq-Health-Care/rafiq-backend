d57897d9f959b7e955807807faf8ef24a9c595b2
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-27 12:30:53 +0300

    Make dashboard configuration dynamic in `JobSchedulerConfig`. Use `@Value` to toggle dashboard based on `jobrunr.dashboard.enabled` property.
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/config/JobSchedulerConfig.java
+++ src/main/java/com/nexaworks/rafiq/config/JobSchedulerConfig.java
@@ -3,9 +3,11 @@ package com.nexaworks.rafiq.config;
 import javax.sql.DataSource;
 
 import org.jobrunr.configuration.JobRunr;
+import org.jobrunr.configuration.JobRunrConfiguration;
 import org.jobrunr.scheduling.JobScheduler;
 import org.jobrunr.server.JobActivator;
 import org.jobrunr.storage.sql.common.SqlStorageProviderFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,9 +15,16 @@ import org.springframework.context.annotation.Configuration;
 public class JobSchedulerConfig {
 
     @Bean
-    public JobScheduler jobScheduler(DataSource dataSource, JobActivator jobActivator) {
-        return JobRunr.configure().useJobActivator(jobActivator)
+    public JobScheduler jobScheduler(DataSource dataSource, JobActivator jobActivator,
+            @Value("${jobrunr.dashboard.enabled:true}") boolean dashboardEnabled) {
+        JobRunrConfiguration configuration = JobRunr.configure().useJobActivator(jobActivator)
                 .useStorageProvider(SqlStorageProviderFactory.using(dataSource))
-                .useBackgroundJobServer().useDashboard().initialize().getJobScheduler();
+                .useBackgroundJobServer();
+
+        if (dashboardEnabled) {
+            configuration.useDashboard();
+        }
+
+        return configuration.initialize().getJobScheduler();
     }
 }
