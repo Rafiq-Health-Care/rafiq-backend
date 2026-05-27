66f8619b885b16a7794318a7b030e99f63dc2c9c
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-27 12:30:00 +0300

    Add mapping method `toProfileResponse` in `DoctorMapper`: map `Doctor` and `DoctorStatsDTO` to `DoctorProfileResponse`.
    Signed-off-by: melbiialy <mahmoudelbialy109@gmail.com>

--- src/main/java/com/nexaworks/rafiq/mapper/DoctorMapper.java
+++ src/main/java/com/nexaworks/rafiq/mapper/DoctorMapper.java
@@ -1,8 +1,17 @@
 package com.nexaworks.rafiq.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.nexaworks.rafiq.dto.response.doctor.DoctorProfileResponse;
+import com.nexaworks.rafiq.dto.response.doctor.DoctorStatsDTO;
+import com.nexaworks.rafiq.entities.Doctor;
 
 @Mapper(componentModel = "spring")
 public interface DoctorMapper {
 
+    @Mapping(target = "yearsOfExperience", source = "doctor.experienceYears")
+    @Mapping(target = "nextAvailable", expression = "java(doctorStatsDTO != null ? doctorStatsDTO.startTime() : null)")
+    @Mapping(target = "consultationCount", expression = "java(doctorStatsDTO != null ? doctorStatsDTO.consultationsCount() : null)")
+    DoctorProfileResponse toProfileResponse(Doctor doctor, DoctorStatsDTO doctorStatsDTO);
 }
